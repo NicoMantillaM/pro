af18973affd2ef5756f2144b937b5ccc400d7f11
NicoMantillaM <160680324+NicoMantillaM@users.noreply.github.com>
2024-02-28 09:36:57 -0500

    feat: :construction: avance 7
    esta es una correcion del codgigo anterior, donde añadi que las promociones aparezcan unicamente  las promociones si el producto selccionado cuenta con estas

--- panad.py
+++ panad.py
@@ -1,63 +1,63 @@
 menu =dict({ 
     "Panes": {
         "Productos":list([
-            {"nombre":"Pan blanco","valor": 2.000},
-            {"nombre":"Pan integral", "valor": 3.000},
-            {"nombre":"Bollos de pan", "valor":2.800},
-            {"nombre":"Baguette", "valor":1.500},
-            {"nombre":"Pan de centeno", "valor":5.000},
-            {"nombre":"Pan de semillas", "valor":1.000},
-            {"nombre":"Pan de ajo", "valor":2.300},
-            {"nombre":"Pan de cebolla", "valor":4.000},
-            {"nombre":"Pan de maíz", "valor":7.000},
-            {"nombre":"Pan de aceitunas", "valor":5.500}
+            {"nombre":"Pan blanco","valor": 2000},
+            {"nombre":"Pan integral", "valor": 3000},
+            {"nombre":"Bollos de pan", "valor":2800},
+            {"nombre":"Baguette", "valor":1500},
+            {"nombre":"Pan de centeno", "valor":5000},
+            {"nombre":"Pan de semillas", "valor":1000},
+            {"nombre":"Pan de ajo", "valor":2300},
+            {"nombre":"Pan de cebolla", "valor":4000},
+            {"nombre":"Pan de maíz", "valor":7000},
+            {"nombre":"Pan de aceitunas", "valor":5500}
         ]),
 
         "promociones":list([ 
-            {"codigo": 0, "nombre": "compre 5", "valor":7.900},
-            {"codigo": 1, "nombre": "compre 3", "valor":5.200}
+            {"codigo": 0, "nombre": "compre 5", "valor":7900},
+            {"codigo": 1, "nombre": "compre 3", "valor":5200}
          ])
 
     },
 
      "Postres": {
         "Productos":list([
-            {"nombre":"Croissants","valor": 1.000},
-            {"nombre":"Magdalenas", "valor": 2.500},
-            {"nombre":"Donas", "valor":1.500},
-            {"nombre":"Tortas", "valor":2.500},
-            {"nombre":"Galletas", "valor":1.000},
-            {"nombre":"Brownies", "valor":1.200},
-            {"nombre":"Empanadas dulces", "valor":2.600},
-            {"nombre":"Rosquillas", "valor":1.300},
-            {"nombre":"Palmeritas", "valor":2.200},
-            {"nombre":"Tartaletas", "valor":3.500}
+            {"nombre":"Croissants","valor": 1000},
+            {"nombre":"Magdalenas", "valor": 2500},
+            {"nombre":"Donas", "valor":1500},
+            {"nombre":"Tortas", "valor":2500},
+            {"nombre":"Galletas", "valor":1000},
+            {"nombre":"Brownies", "valor":1200},
+            {"nombre":"Empanadas dulces", "valor":2600},
+            {"nombre":"Rosquillas", "valor":1300},
+            {"nombre":"Palmeritas", "valor":2200},
+            {"nombre":"Tartaletas", "valor":3500}
         ]),
 
         "promociones":list([ 
-            {"codigo": 0, "nombre": "compre 4", "valor":8.000},
-            {"codigo": 1, "nombre": "compre 7", "valor":10.000}
+            {"codigo": 0, "nombre": "compre 4", "valor":8000},
+            {"codigo": 1, "nombre": "compre 7", "valor":10000}
          ])
         
     },
 
      "Galletas": {
         "Productos":list([
-            {"nombre":"Galletas de chocolate","valor": 1.400},
-            {"nombre":"Galletas de avena", "valor": 1.200},
-            {"nombre":"Galletas de mantequilla", "valor":1.000},
-            {"nombre":"Galletas de jengibre", "valor":1.500},
-            {"nombre":"Galletas de almendras", "valor":1.200},
-            {"nombre":"Galletas de nueces", "valor":1.500},
-            {"nombre":"Galletas de coco", "valor":1.000},
-            {"nombre":"Galletas de pasas", "valor":1.300},
-            {"nombre":"Galletas de miel", "valor":1.500},
-            {"nombre":"Galletas de vainilla", "valor":1.200}
+            {"nombre":"Galletas de chocolate","valor": 1400},
+            {"nombre":"Galletas de avena", "valor": 1200},
+            {"nombre":"Galletas de mantequilla", "valor":1000},
+            {"nombre":"Galletas de jengibre", "valor":1500},
+            {"nombre":"Galletas de almendras", "valor":1200},
+            {"nombre":"Galletas de nueces", "valor":1500},
+            {"nombre":"Galletas de coco", "valor":1000},
+            {"nombre":"Galletas de pasas", "valor":1300},
+            {"nombre":"Galletas de miel", "valor":1500},
+            {"nombre":"Galletas de vainilla", "valor":1200}
         ]),
 
         "promociones":list([ 
-            {"codigo": 0, "nombre": "compre 6", "valor":5.000},
-            {"codigo": 1, "nombre": "compre 3", "valor":3.900}
+            {"codigo": 0, "nombre": "compre 6", "valor":5000},
+            {"codigo": 1, "nombre": "compre 3", "valor":3000}
          ]),
      
     },
@@ -77,33 +77,28 @@ opcionCategoria = int(input())
 #obtener los datos de la categoria seleccionada
 datosCategoria = menu.get(listaCategoria[opcionCategoria])
 productosCategoria=datosCategoria["Productos"]
-promocionesCategoria=datosCategoria["promociones"]
 
-print (f"usuario usted selecciono la categoria {listaCategoria[opcionCategoria]} ")
+#correcion segun clase
+print(f"seleccione el producto de la categoria{listaCategoria[opcionCategoria]}")
+for i, val in enumerate(productosCategoria):
+  print(f"{i}. {val}")
+opcionproducto = int(input())
+datosCategoria = menu.get(listaCategoria[opcionCategoria])
+promocionesProducto=datosCategoria["promociones"]
 
-#mostrar los productos de la categoria seleccionada
-print ("\nProductos disponibles:")
-for producto in productosCategoria:
-    print("- Nombre:", producto["nombre"], "| Valor:", producto["valor"])
+promocionesProductos = list()
+for val in promocionesProducto:
+    if (val.get("codigo") == opcionproducto):
+        promocionesProductos.append(val)
 
-#mostrar las promociones de la categoria seleccionada
-print ("\npromociones disponibles:")  
-for promocion in promocionesCategoria:
-    print("- Nombre:", promocion["nombre"], "| Valor:", promocion["valor"]) 
+if (len(promocionesProductos) == 0):
+    print(f"No hay promociones para el producto{datosCategoria['Productos'][opcionproducto]}")   
+else:  
+    print(f"Promociones del producto{datosCategoria['Productos'][opcionproducto]}") 
+    print (promocionesProducto) 
 
-#permitir al usuario elegir el producto o la promocion de la categoria
-escogProductoPromo = input("Seleccione el número del producto/promoción que desea adquirir:")
 
  
-
-
-
-
-
-
-
-
-
 
 
 
